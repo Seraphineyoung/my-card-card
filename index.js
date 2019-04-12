924fc4ff8948a5c393c1e312f90019ebe16a4cf1
Seraphine Young <seraphine_ene@yahoo.com>
2019-04-12 10:26:36 +0100

    Made changes to routing

--- index.js
+++ index.js
@@ -2,19 +2,20 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const path = require("path");
+const apiRoutes = require("./routes");
+const API_ROOT_PATH = "/";
+
 app.use(express.json());
 app.use(cors());
 
-// const API_ROOT_PATH = "/api";
-// const apiRoutes = require("./routes");
-// app.use(API_ROOT_PATH, apiRoutes);
+app.use(API_ROOT_PATH, apiRoutes);
 
-// app.get("*", (req, res, next) => {
-//   if (req.url.startsWith(API_ROOT_PATH)) {
-//     return next();
-//   }
-//   res.sendFile(path.join(__dirname, "public/index.html"));
-// });
+app.get("*", (req, res, next) => {
+  if (req.url.startsWith(API_ROOT_PATH)) {
+    return next();
+  }
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
 
 app.use(express.static(path.join(__dirname, "crazy-cards-fe/build")));
 
